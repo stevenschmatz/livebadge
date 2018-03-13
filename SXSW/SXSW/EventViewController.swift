21b005f009212ff80cae76f522ad1baa2d97a579
Stuart Olivera <stuart@stuartolivera.com>
2018-03-13 17:23:16 -0500

    Add event view & popover

---
+++ SXSW/SXSW/EventViewController.swift
@@ -0,0 +1,33 @@
+//
+//  EventViewController.swift
+//  SXSW
+//
+//  Created by Stuart Olivera on 3/13/18.
+//  Copyright © 2018 Stuart Olivera. All rights reserved.
+//
+
+import UIKit
+
+class EventViewController: UIViewController {
+
+    @IBAction func done(_ sender: Any) {
+        self.dismiss(animated: true)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+}
