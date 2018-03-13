f6f2783b31f6a548fff53be51a7d270fe30c958e
Stuart Olivera <stuart@stuartolivera.com>
2018-03-13 18:09:39 -0500

    Initial work on table view controller

---
+++ SXSW/SXSW/ItemTableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ItemTableViewController.swift
+//  SXSW
+//
+//  Created by Stuart Olivera on 3/13/18.
+//  Copyright © 2018 Stuart Olivera. All rights reserved.
+//
+
+import UIKit
+
+class ItemTableViewController : UITableViewController {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1;
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1;
+    }
+}
