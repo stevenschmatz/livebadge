f26472f0d4d26c2eb77bd302cb57e1b89b54dfa9
Stuart Olivera <stuart@stuartolivera.com>
2018-03-13 20:16:18 -0500

    Alternate colors

--- SXSW/SXSW/Constants.swift
+++ SXSW/SXSW/Constants.swift
@@ -14,3 +14,24 @@ struct Colors {
     static let Pink = UIColor(red: 245/255.0, green: 113/255.0, blue: 209/255.0, alpha: 1)
     static let Orange = UIColor(red: 242/255.0, green: 145/255.0, blue: 32/255.0, alpha: 1)
 }
+
+// https://stackoverflow.com/questions/24263007/how-to-use-hex-colour-values
+extension UIColor {
+    convenience init(hexString: String) {
+        let hex = hexString.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int = UInt32()
+        Scanner(string: hex).scanHexInt32(&int)
+        let a, r, g, b: UInt32
+        switch hex.count {
+        case 3: // RGB (12-bit)
+            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: // RGB (24-bit)
+            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8: // ARGB (32-bit)
+            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (a, r, g, b) = (255, 0, 0, 0)
+        }
+        self.init(red: CGFloat(r) / 255, green: CGFloat(g) / 255, blue: CGFloat(b) / 255, alpha: CGFloat(a) / 255)
+    }
+}
